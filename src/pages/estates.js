70a8aec2c3923fe7d952295c2bec190220054bee
Daniel Krux <danielmartijn@gmail.com>
2020-01-09 14:51:26 +0100

    working on detailpage

--- src/pages/estates.js
+++ src/pages/estates.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 import Layout from '../components/layout'
 import estatesStyles from "../styles/estates.module.scss"
--- src/templates/estate.js
+++ src/templates/estate.js
@@ -4,6 +4,8 @@ import Layout from "../components/layout"
 import eS from "../styles/estate.module.scss"
 import Carousel from "../components/estate/carousel"
 import RichText from "../components/estate/richText"
+import { FaCloudSun, FaTree, FaSwimmingPool, FaHotjar, FaParking, FaBoxes, FaWind } from "react-icons/fa"
+import '../styles/index.scss'
 
 export const query = graphql`
   query($reference: String!) {
@@ -44,6 +46,31 @@ export const query = graphql`
 
 let converter = Intl.NumberFormat("nl")
 
+function setIcon(edge) {
+  switch (edge) {
+    case "uitzicht":
+      return <FaCloudSun size={25} />
+    case "tuin":
+      return <FaTree size={25} />
+    case "zwembad":
+      return <FaSwimmingPool size={25} />
+    case "verwarming":
+      return <FaHotjar size={25} />
+    case "airconditioning":
+      return <FaWind size={25} />
+    case "parkeerplaats":
+      return <FaParking size={25} />
+    case "berging":
+      return <FaBoxes size={25} />
+    default:
+      break;
+  }
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 const EstateDetail = props => {
   const estate = props.data.contentfulSilcEstate
   return (
@@ -52,7 +79,10 @@ const EstateDetail = props => {
       <article className={eS.container}>
         <section className={eS.title}>
           <h3>{estate.title}</h3>
-          <p>{estate.reference}</p>
+          <div classname={eS.rowSpaceBetween}>
+            <p>{estate.reference}</p>
+            <p>{capitalizeFirstLetter(estate.estateType)}</p>
+          </div>
         </section>
 
         <section className={eS.price}>
@@ -62,8 +92,11 @@ const EstateDetail = props => {
         <section className={eS.amentities}>
           {estate.amentities.map((edge, i) => {
             return (
-              <div className={eS.amentity} key={i}>
-                {edge}
+              <div className={eS.amentity}>
+                {setIcon(edge)}
+                <div key={i}>
+                  <span>{capitalizeFirstLetter(edge)}</span>
+                </div>
               </div>
             )
           })}
@@ -75,7 +108,25 @@ const EstateDetail = props => {
         </section>
 
         <section className={eS.estateData}>
-          
+          <div>
+            <div className={eS.roomCard}>
+              <p>Slaapkamers</p>
+              <span>{estate.bedrooms}</span>
+            </div>
+          </div>
+          <div>
+            <div className={eS.roomCard}>
+              <p>Badkamers</p>
+              <span>{estate.bathrooms}</span>
+            </div>
+          </div>
+          <div>
+            <p><b>Bouwjaar:</b> {estate.yearOfConstruction}</p>
+            <p><b>Woning:</b> {estate.sizeLivingSpace} M<sup>2</sup></p>
+            <p><b>Terras:</b> {estate.sizeTerrace} M<sup>2</sup></p>
+            <p><b>Perceel:</b> {estate.sizeOfPlot} M<sup>2</sup></p>
+            <p><b>Solarium:</b> {estate.sizeSolarium} M<sup>2</sup></p>
+          </div>
         </section>
       </article>
     </Layout>
