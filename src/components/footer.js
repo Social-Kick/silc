af42bde9f78cbb7e5e1670ca83ab4f7fbdc65e1b
Daniel Krux <daniel@socialkick.be>
2020-06-02 18:22:57 +0200

    chore: one more BV in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -52,7 +52,7 @@ const Footer = () => {
         <Desktop><p>●</p></Desktop>
         <p><a href={cp} target="__blank">Cookie policy</a></p>
         <Desktop><p>●</p></Desktop>
-        <p>BIV 800 007 Bruyninckx Vastgoed</p>
+        <p>BIV 800 007 Bruyninckx Vastgoed BV</p>
         <Desktop><p>●</p></Desktop>
         <p>BIV 203 854 Carl Bruyninckx</p>
       </div>
