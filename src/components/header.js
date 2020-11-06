a9af32d275200f514a00a50e25acd7d13c7433b0
Stijn Swaanen <swaanenstijn@gmail.com>
2020-11-06 18:04:23 +0100

    feat(costa): dynamic costa sub-menu

--- src/components/header.js
+++ src/components/header.js
@@ -7,9 +7,8 @@ import chevron from '../images/chevron-down-purple.svg';
 
 import headerStyles from "../styles/components/header.module.scss";
 
-const Nav = () => {
+const Nav = ({ costaMenuItems }) => {
   const [toggleCosta, setToggleCosta] = useState(false);
-
 
   return (
     <React.Fragment>
@@ -30,15 +29,14 @@ const Nav = () => {
               <img src={chevronWhite} alt="chevron" className={headerStyles.chevron}/>
             </div>
             {toggleCosta && <ul className={headerStyles.subNavMenu}>
-              <li>
-                <Link style={{ fontSize: '1.4rem' }} activeClassName={headerStyles.active} to="/costa/costa-blanca-zuid" className={headerStyles.navItem}>Costa Blanca Zuid</Link>
-              </li>
-              <li>
-                <Link style={{ fontSize: '1.3rem' }} activeClassName={headerStyles.active} to="/costa/costa-blanca-noord" className={headerStyles.navItem}>Costa Blanca Noord</Link>
-              </li>
-              <li>
-                <Link style={{ fontSize: '1.6rem' }} activeClassName={headerStyles.active} to="/costa/costa-del-sol" className={headerStyles.navItem}>Costa Del Sol</Link>
-              </li>
+              {costaMenuItems.map(({ titel }) => {
+                const formattedCostaReference = titel.replace(/\s+/g, '-').toLowerCase().replace('?', '').replace('#', '');
+                return (
+                  <li>
+                    <Link style={{ fontSize: '1.4rem' }} activeClassName={headerStyles.active} to={`/costa/${formattedCostaReference}`} className={headerStyles.navItem}>{titel}</Link>
+                  </li>
+                );  
+              })}
             </ul>}
           </li>
           <li>
@@ -65,119 +63,111 @@ const Nav = () => {
   )
 }
 
-class Header extends Component {
-  constructor(props, context) {
-    super(props, context)
-    this.state = {
-      isExpanded: null,
-      costaExpanded: false
-    };
+const Header = ({ costaMenuItems }) => {
+  const [isExpanded, setIsExpanded] = useState(null);
+  const [costaExpanded, setCostaExpanded] = useState(false);
+
+  const toggle = () => {
+    setIsExpanded(!isExpanded);
   }
 
-  toggle = () => {
-    this.setState(prevState => ({ isExpanded: !prevState.isExpanded }));
+  const toggleCosta = () => {
+    setCostaExpanded(!costaExpanded);
   }
 
-  toggleCosta = () => {
-    this.setState(prevState => ({ costaExpanded: !prevState.costaExpanded }));
-  }
+  return (
+    <div>
+      <header className="no-print">
+        <Link to="/">
+          <img className={headerStyles.logo} src="https://silcestates.com/wp-content/uploads/2019/07/logo-SILC-states.png" alt="logo"></img>
+        </Link>
 
-  render() {
-    return (
-      <div>
-        <header className="no-print">
-          <Link to="/">
-            <img className={headerStyles.logo} src="https://silcestates.com/wp-content/uploads/2019/07/logo-SILC-states.png" alt="logo"></img>
-          </Link>
-
-          <Mobile>
-            <button className={headerStyles.btn} onClick={this.toggle}>
-              <FontAwesomeIcon icon={['fal', 'bars']} />
-            </button>
-          </Mobile>
-          <Tablet>
-            <button className={headerStyles.btn} onClick={this.toggle}>
-              <FontAwesomeIcon icon={['fal', 'bars']} />
-            </button>
-          </Tablet>
-          <Desktop>
-            <nav className={`${headerStyles.desktopNav}`}>
-              <ul className={headerStyles.navList}>
-                <li>
-                  <Link activeClassName={headerStyles.active} to="/" className={headerStyles.navItem}>Home</Link>
-                </li>
-                <li>
-                  <Link activeClassName={headerStyles.active} to="/estates" className={headerStyles.navItem}>Aanbod</Link>
-                </li>
-                <li>
-                  <Link activeClassName={headerStyles.active} to="/trip" className={headerStyles.navItem}>Bezichtigingsreis</Link>
-                </li>
-                <li>
-                  <div className={headerStyles.subMenuItem} onClick={this.toggleCosta}>
-                    Costa's
-                    <img src={chevron} alt="chevron" className={headerStyles.chevron}/>
-                  </div>
-                  {this.state.costaExpanded && <ul className={headerStyles.subNavMenu}>
-                    <li>
-                      <Link activeClassName={headerStyles.active} to="/costa/costa-blanca-zuid" className={headerStyles.navItem}>Costa Blanca Zuid</Link>
-                    </li>
-                    <li>
-                      <Link activeClassName={headerStyles.active} to="/costa/costa-blanca-noord" className={headerStyles.navItem}>Costa Blanca Noord</Link>
-                    </li>
-                    <li>
-                      <Link activeClassName={headerStyles.active} to="/costa/costa-del-sol" className={headerStyles.navItem}>Costa Del Sol</Link>
-                    </li>
-                  </ul>}
-                </li>
-                <li>
-                  <Link activeClassName={headerStyles.active} to="/about" className={headerStyles.navItem}>Over ons</Link>
-                </li>
-                <li>
-                  <Link activeClassName={headerStyles.active} to="/services" className={headerStyles.navItem}>Diensten</Link>
-                </li>
-                <li>
-                  <Link activeClassName={headerStyles.active} to="/blog" className={headerStyles.navItem}>Nieuws</Link>
-                </li>
-                <li>
-                  <Link activeClassName={headerStyles.active} to="/tips" className={headerStyles.navItem}>Tips & Tricks</Link>
-                </li>
-                <li>
-                  <Link activeClassName={headerStyles.active} to="/wijzoekenvoorjou" className={headerStyles.navItem}>Wij zoeken voor jou</Link>
-                </li>
-                <li>
-                  <Link activeClassName={headerStyles.active} to="/contact" className={headerStyles.navItem}>Contact</Link>
-                </li>
-              </ul>
-            </nav>
-          </Desktop>
-        </header>
-
-        <Tablet>
-          {this.state.isExpanded &&
-            <div className={headerStyles.fullscreen}>
-              <div className={headerStyles.topRow}>
-                <img src="https://silcestates.com/wp-content/uploads/2019/07/logo_contouren_wit-copia-1.png" />
-                <button onClick={this.toggle} className={headerStyles.closeFullscreen}><FontAwesomeIcon icon={['fal', 'times']} size={"2x"} /></button>
-              </div>
-              <Nav className={headerStyles.nav} />
-            </div>
-          }
-        </Tablet>
         <Mobile>
-          {this.state.isExpanded &&
-            <div className={headerStyles.fullscreen}>
-              <div className={headerStyles.topRow}>
-                <img src="https://silcestates.com/wp-content/uploads/2019/07/logo_contouren_wit-copia-1.png" />
-                <button onClick={this.toggle} className={headerStyles.closeFullscreen}><FontAwesomeIcon icon={['fal', 'times']} size={"2x"} /></button>
-              </div>
-              <Nav className={headerStyles.nav} />
-            </div>
-          }
+          <button className={headerStyles.btn} onClick={toggle}>
+            <FontAwesomeIcon icon={['fal', 'bars']} />
+          </button>
         </Mobile>
+        <Tablet>
+          <button className={headerStyles.btn} onClick={toggle}>
+            <FontAwesomeIcon icon={['fal', 'bars']} />
+          </button>
+        </Tablet>
+        <Desktop>
+          <nav className={`${headerStyles.desktopNav}`}>
+            <ul className={headerStyles.navList}>
+              <li>
+                <Link activeClassName={headerStyles.active} to="/" className={headerStyles.navItem}>Home</Link>
+              </li>
+              <li>
+                <Link activeClassName={headerStyles.active} to="/estates" className={headerStyles.navItem}>Aanbod</Link>
+              </li>
+              <li>
+                <Link activeClassName={headerStyles.active} to="/trip" className={headerStyles.navItem}>Bezichtigingsreis</Link>
+              </li>
+              <li>
+                <div className={headerStyles.subMenuItem} onClick={toggleCosta}>
+                  Costa's
+                  <img src={chevron} alt="chevron" className={headerStyles.chevron}/>
+                </div>
+                {costaExpanded && <ul className={headerStyles.subNavMenu}>
+                  {costaMenuItems.map(({ titel }) => {
+                    const formattedCostaReference = titel.replace(/\s+/g, '-').toLowerCase().replace('?', '').replace('#', '');
+                    return (
+                      <li>
+                        <Link activeClassName={headerStyles.active} to={`/costa/${formattedCostaReference}`} className={headerStyles.navItem}>{titel}</Link>
+                      </li>
+                    );  
+                  })}
+                </ul>}
+              </li>
+              <li>
+                <Link activeClassName={headerStyles.active} to="/about" className={headerStyles.navItem}>Over ons</Link>
+              </li>
+              <li>
+                <Link activeClassName={headerStyles.active} to="/services" className={headerStyles.navItem}>Diensten</Link>
+              </li>
+              <li>
+                <Link activeClassName={headerStyles.active} to="/blog" className={headerStyles.navItem}>Nieuws</Link>
+              </li>
+              <li>
+                <Link activeClassName={headerStyles.active} to="/tips" className={headerStyles.navItem}>Tips & Tricks</Link>
+              </li>
+              <li>
+                <Link activeClassName={headerStyles.active} to="/wijzoekenvoorjou" className={headerStyles.navItem}>Wij zoeken voor jou</Link>
+              </li>
+              <li>
+                <Link activeClassName={headerStyles.active} to="/contact" className={headerStyles.navItem}>Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </Desktop>
+      </header>
 
-      </div>
-    )
-  }
+      <Tablet>
+        {isExpanded &&
+          <div className={headerStyles.fullscreen}>
+            <div className={headerStyles.topRow}>
+              <img src="https://silcestates.com/wp-content/uploads/2019/07/logo_contouren_wit-copia-1.png" />
+              <button onClick={toggle} className={headerStyles.closeFullscreen}><FontAwesomeIcon icon={['fal', 'times']} size={"2x"} /></button>
+            </div>
+            <Nav costaMenuItems={costaMenuItems} className={headerStyles.nav} />
+          </div>
+        }
+      </Tablet>
+      <Mobile>
+        {isExpanded &&
+          <div className={headerStyles.fullscreen}>
+            <div className={headerStyles.topRow}>
+              <img src="https://silcestates.com/wp-content/uploads/2019/07/logo_contouren_wit-copia-1.png" />
+              <button onClick={toggle} className={headerStyles.closeFullscreen}><FontAwesomeIcon icon={['fal', 'times']} size={"2x"} /></button>
+            </div>
+            <Nav costaMenuItems={costaMenuItems} className={headerStyles.nav} />
+          </div>
+        }
+      </Mobile>
+
+    </div>
+  )
 }
 
 
